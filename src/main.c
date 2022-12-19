df38b128c7c25d8cc005b474f4e8a6d2dff4c7d8
sebastianguerra <spguerraespinoza@gmail.com>
2022-12-19 18:22:01 -0300

    Ahora los fps se controlan por un delta del tiempo en vez de un sleep

--- src/main.c
+++ src/main.c
@@ -1,14 +1,53 @@
 #include <string.h>
 #include <unistd.h>
 #include <ncurses.h>
+#include <time.h>
 
 
 #include "game.h"
 
 
 
-void drawScreen(int w, int h, byte matix[w][h], int rows, int columns, int x, int y, int fps);
+void drawScreen(int w, int h, byte matix[w][h], int rows, int columns, int x, int y, int fps, long delta_ms);
 void show (int w, int h, byte matrix[w][h], int rows, int columns, int x, int y);
+
+
+
+
+
+
+
+
+
+
+
+enum { NS_PER_S  = 1000000000,
+	   NS_PER_MS = 1000000,
+	   MS_PER_S  = 1000 };
+
+void sub_timespec(struct timespec t1, struct timespec t2, struct timespec *td)
+{
+    td->tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    td->tv_sec  = t2.tv_sec - t1.tv_sec;
+    if (td->tv_sec > 0 && td->tv_nsec < 0)
+    {
+        td->tv_nsec += NS_PER_S;
+        td->tv_sec--;
+    }
+    else if (td->tv_sec < 0 && td->tv_nsec > 0)
+    {
+        td->tv_nsec -= NS_PER_S;
+        td->tv_sec++;
+    }
+}
+
+void timespec2ms(struct timespec t, long *r) {
+	*r = t.tv_nsec/NS_PER_MS;
+	*r += t.tv_sec*MS_PER_S;
+}
+
+
+
 
 
 
@@ -89,55 +128,83 @@ int main() {
 	// x, y: Posicion actual de la vista
 	int x = 0, y = 0;
 
-	for (int i = 0; ; i+=running) {
-		int c = getch();
-		switch(c){
+
+
+	struct timespec now, lastFrame, delta;
+	clock_gettime(CLOCK_REALTIME, &now);
+	clock_gettime(CLOCK_REALTIME, &lastFrame);
+
+	///////////////////////////////////////////////////////
+	//// Loop principal
+	///////////////////////////////////////////////////////
+	int iter = 0;
+	for (;;) {
+		clock_gettime(CLOCK_REALTIME, &now);
+
+		sub_timespec(lastFrame, now, &delta);
+		long delta_ms;
+		timespec2ms(delta, &delta_ms);
+
+		int curr = iter%2;
+
+		switch(getch()){
+			// Opciones de movimiento de pantalla
 			case 'h':
 				y++;
-				drawScreen(w, h, state[(i/10)%2], row, col, x, y, fps);
-				break;
+				goto REDRAW;
 			case 'j':
 				x--;
-				drawScreen(w, h, state[(i/10)%2], row, col, x, y, fps);
-				break;
+				goto REDRAW;
 			case 'k':
 				x++;
-				drawScreen(w, h, state[(i/10)%2], row, col, x, y, fps);
-				break;
+				goto REDRAW;
 			case 'l':
 				y--;
-				drawScreen(w, h, state[(i/10)%2], row, col, x, y, fps);
-				break;
+				goto REDRAW;
 
+			// Opciones para modificar FPS
 			case '-':
 				fps-=1;
-				break;
+				goto REDRAW;
 			case '+':
 				fps+=1;
-				break;
+				goto REDRAW;
 			
+			// Pausar/resumir simulacion
 			case 'p':
 				running = !running;
-				break;
+				goto REDRAW;
 			
+			// Dibujar de nuevo
+			case 'r':
+				goto REDRAW;
+
+			// Salir
 			case 'q':
 				goto end_loop;
+		}
+
+		if(0) {
+			REDRAW:
+			drawScreen(w, h, state[curr], row, col, x, y, fps, delta_ms);
 		}
 
 		flushinp(); // Evita que quede input almacenado al mantener una tecla
 					// apretada y que al soltarla la siga recibiendo
 		getmaxyx(stdscr, row, col); // reinica tamano terminal por si se modifico
 
-		//// Realiza una iteracion del juego
-		if (i%10 == 0) {
-			drawScreen(w, h, state[(i/10)%2], row, col, x, y, fps);
-			if (running)
-				compute(w, h, state[(i/10)%2], state[((i/10)+1)%2]);
-
+		// Realiza una iteracion del juego
+		if (delta_ms >= 1000/fps) {
+			if (running){
+				drawScreen(w, h, state[curr], row, col, x, y, fps, delta_ms);
+				compute(w, h, state[curr], state[!curr]);
+				iter++;
+				lastFrame = now;
+			}
 		}
 
 		// delay
-		napms(1000/fps /10);
+		napms(1);
 	}
 
 	end_loop:
@@ -147,9 +214,10 @@ int main() {
 }
 
 
-void drawScreen(int w, int h, byte matrix[w][h], int rows, int columns, int x, int y, int fps) {
+void drawScreen(int w, int h, byte matrix[w][h], int rows, int columns, int x, int y, int fps, long delta_ms) {
 	show(w, h, matrix, rows, columns, x, y);
-	mvprintw(rows-1, columns-10, "FPS: %d", fps);
+	mvprintw(rows-2, columns-15, "  FPS: %d", fps);
+	mvprintw(rows-1, columns-15, "Delta: %ld", delta_ms);
 	refresh();
 
 }
